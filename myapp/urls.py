e60a17bb3168399d78c8fb2e41b59da3b2c80642
obeliskB <anusorn.b@gmail.com>
2023-09-26 11:15:20 +0700

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('', views.index),
+    path('hello/', views.hello),
+    path('db/', views.db),
+    path('add/', views.add),
+    path('update/<product_id>', views.update),
+    path('delete/<product_id>', views.delete),
+]
